95ac2f3017589f36473e3d82f72ce213bc893ce3
Levon-MJ <Levon093612mail.ru>
2021-10-05 19:33:05 +0300

    changet ifExample, add IntExample

--- src/lesson4/Example4.java
+++ src/lesson4/Example4.java
@@ -2,8 +2,35 @@ package lesson4;
 
 public class Example4 {
     public static void main(String[] args) {
-        int a = 5;
-        System.out.println("a=" + a);
+        char L = 76;
+        char e = 101;
+        char v = 118;
+        char o = 111;
+        char n = 110;
+        System.out.println(L);
+        System.out.println(e);
+        System.out.println(v);
+        System.out.println(o);
+        System.out.println(n);
+
+        char L1 = 'l';
+        System.out.println(L1);
+
+        boolean a = 50 > 100;
+        System.out.println(a); //folse
+
+        int y = 999_888_777;
+        System.out.println(y);
+
+        char t='\"';
+        System.out.println(t);
+
+        System.out.println("Levon\nAvetisyan");
+
+
+
+
+
 
 
     }
